e8661448ec4da9a310b69836b7d22eaeb543cef3
AlessandroPiccolo <alessandro.piccolo@hotmail.se>
2018-05-27 22:57:40 +0200

    Add flask api

--- src/car-pooling-backend/car-pooling-backend.py
+++ src/car-pooling-backend/car-pooling-backend.py
@@ -1,35 +1,12 @@
-from celery import Celery
+from flask import Flask
+app = Flask(__name__)
 
 
-class FlaskCelery(Celery):
-    """
-    Flask aware Celery class. Calls tasks with a new app context for each task call.
-    """
+@app.route("/")
+def hello():
+    return "Hello World!"
 
-    def __init__(self, *args, **kwargs):
-        super(FlaskCelery, self).__init__(*args, **kwargs)
-        self.patch_task()
 
-        if 'app' in kwargs:
-            self.init_app(kwargs['app'])
-
-    def patch_task(self):
-        TaskBase = self.Task
-        _celery = self
-
-        class ContextTask(TaskBase):
-            abstract = True
-
-            def __call__(self, *args, **kwargs):
-                with _celery.app.app_context():
-                    return TaskBase.__call__(self, *args, **kwargs)
-
-        self.Task = ContextTask
-
-    def configure(self):
-        self.conf.task_default_queue = self.app.config['SERVICE_NAME']
-
-    def init_app(self, app):
-        self.app = app
-        self.conf.broker_url = app.config['BROKER_URI']
-        self.configure()
+@app.route("/score")
+def score():
+    return "Score!"
